3993fed055e2c6f3d4085dcef76248950c5e5753
Nathan <nathan.huse@yahoo.com>
2021-09-26 11:00:32 -0500

    Added comments

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,8 +6,10 @@ import logo from './styles/IMG_0254.jpg'
 
 export default function NavBar({ user, setUser, setGameId }) {
     const history = useHistory()
-    let userImg;
+    let userImg = 'https://i.imgur.com/9UfDphN.jpg'
+
     function handleLogoutClick() {
+        // Logout functionality. Deletes session cookie from the back end.
         fetch('/logout', {
             method: "DELETE"
         })
@@ -20,16 +22,10 @@ export default function NavBar({ user, setUser, setGameId }) {
         });          
     }
 
-    // function handleClick() {
-    //     setGameId(null)
-    // }
-    if(user){
-        if(user.profile_img) {
-            userImg = user.profile_img
-        } else {
-            userImg = 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E'
-        }
+    if(user.profile_img) {
+        userImg = user.profile_img
     }
+    
     return (
         <nav className="nav-bar" >
             { user ? 
